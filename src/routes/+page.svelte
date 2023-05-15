73708f0749951ad9547a2a486293e0c0e6fcb996
v93938 <ovanluchene@vanmarcke.be>
2023-05-15 17:45:15 +0200

    Navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,21 @@
-<div class="m-10">
-  <h1 class="text-3xl font-bold underline">Tailwind Playground</h1>
-</div>
+<nav class="flex items-center justify-between flex-wrap bg-sky-950 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <span class="font-semibold text-xl tracking-tight">Tailwind Playground</span>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-orange-300 mr-4">
+        Docs
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-orange-300 mr-4">
+        Examples
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-orange-300">
+        Blog
+      </a>
+    </div>
+  </div>
+</nav>
 
 <style lang="postcss">
   :global(html) {
