001592338236605b4da22ae5bf12820a617e795a
v93938 <ovanluchene@vanmarcke.be>
2023-05-15 17:24:25 +0200

    inite

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="m-10">
+  <h1 class="text-3xl font-bold underline">Tailwind Playground</h1>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
